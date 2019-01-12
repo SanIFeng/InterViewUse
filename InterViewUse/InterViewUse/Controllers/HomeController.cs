6c2fbb2095e90209f544311ad546037261f4e395
sunkesi02 <sunkesi02@yahoo.com.tw>
2019-01-12 16:47:25 +0800

    建立首頁查詢所有的資料
    建立首頁查詢所有的資料

--- InterViewUse/InterViewUse/Controllers/HomeController.cs
+++ InterViewUse/InterViewUse/Controllers/HomeController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InterViewUse.Models;
 
 namespace InterViewUse.Controllers
 {
     public class HomeController : Controller
     {
+        Repository<Shippers> shippers = new Repository<Shippers>();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(shippers.GetAll());
         }
     }
 }
