6c0baa31a92491d83ddd268149d11476cfafb0a5
sunkesi02 <sunkesi02@yahoo.com.tw>
2019-01-12 16:42:22 +0800

    修改首頁樣式、加入Model、資料庫
    修改首頁樣式、加入資料庫mdf、建立Model

---
+++ InterViewUse/InterViewUse/Models/Partial/Shippers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InterViewUse.Models
+{
+    [MetadataType(typeof(MatadataUse))]
+    public partial class Shippers
+    {
+        public class MatadataUse
+        {   [DisplayName("運輸公司編號")]
+            public int ShipperID { get; set; }
+            [DisplayName("運輸公司名稱")]
+            public string CompanyName { get; set; }
+            [DisplayName("運輸公司電話")]
+            public string Phone { get; set; }
+        }
+    }
+}
---
+++ InterViewUse/InterViewUse/Models/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace InterViewUse.Models
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        NorthwindEntities db = null;
+        DbSet<T> Dbset = null;
+        public Repository()
+        {
+            db = new NorthwindEntities();
+            Dbset = db.Set<T>();
+        }
+        public void Create(T _entity)
+        {
+            db.Entry(_entity).State = EntityState.Added;
+            db.SaveChanges();
+        }
+
+        public void Delete(T _entity)
+        {
+            db.Entry(_entity).State = EntityState.Deleted;
+            db.SaveChanges();
+        }
+
+        public void Update(T _entity)
+        {
+            db.Entry(_entity).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Dbset;
+        }
+
+        public T GetByID(int id)
+        {
+            return Dbset.Find(id);
+        }       
+    }
+}
