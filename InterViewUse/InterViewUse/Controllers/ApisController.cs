bc718d93727018568e60b7d36119f2af8e920881
sunkesi02 <sunkesi02@yahoo.com.tw>
2019-01-13 13:43:22 +0800

    建立Api修改資料功能
    建立Api修改資料功能

--- InterViewUse/InterViewUse/Controllers/ApisController.cs
+++ InterViewUse/InterViewUse/Controllers/ApisController.cs
@@ -10,10 +10,12 @@ namespace InterViewUse.Controllers
 {
     public class ApisController : ApiController
     { Repository<Shippers> shippers = new Repository<Shippers>();
+        //==============================取得所有資料=========================================
         public IEnumerable<Shippers> GetAll()
         {
             return shippers.GetAll();
         }
+        //===============================新增一筆資料=====================================
         public IHttpActionResult PostNewone(Shippers sh)
         {
             if (!ModelState.IsValid)//發生綁定前值為null，判斷值是否為有效
@@ -23,8 +25,20 @@ namespace InterViewUse.Controllers
             else
             {   shippers.Create(sh);
                 return StatusCode(HttpStatusCode.Created);//回傳值為201
+            }           
+        }
+        //=================================修改一筆資料=================================
+        public IHttpActionResult PutShipper(Shippers sh)
+        {
+            if (!ModelState.IsValid)//發生綁定前值為null，判斷值是否為有效
+            {
+                return BadRequest(ModelState);//回傳代碼400(值無效)
             }
-            
+            else
+            {
+                shippers.Update(sh);
+                return StatusCode(HttpStatusCode.NoContent);//回傳值為204
+            }
         }
     }
 }
