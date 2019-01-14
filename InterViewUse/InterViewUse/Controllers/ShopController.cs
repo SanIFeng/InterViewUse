ea8fbb4f91860d137ce9ba8d49bce48f2379f134
sunkesi02 <sunkesi02@yahoo.com.tw>
2019-01-14 18:57:08 +0800

    建立結帳功能

--- InterViewUse/InterViewUse/Controllers/ShopController.cs
+++ InterViewUse/InterViewUse/Controllers/ShopController.cs
@@ -10,12 +10,12 @@ namespace InterViewUse.Controllers
     public class ShopController : Controller
     {
         Repository<Categories> categories = new Repository<Categories>();
-        Repository<Products> products = new Repository<Products>();
-        ShopUse shopuse = new ShopUse();
+        Repository<Products> products = new Repository<Products>();        
         // GET: Shop
         //=================================商城首頁===============================================
         public ActionResult Index(int id = 0)
         {
+            ShopUse shopuse = new ShopUse();
             shopuse.categories = categories.GetAll();
             if (id == 0)
             {
@@ -51,13 +51,24 @@ namespace InterViewUse.Controllers
             }
         }
         //=========================================購物車結帳頁面=========================================
-        public ActionResult Accounting()
-        {
-            shopuse.shopping_car = Session["shopping_car"] as List<int>;
-            return View(shopuse);
+        public ActionResult Accounting(int id=0)
+        {  if (id == 0)
+            {
+                ShopUse shopuse = new ShopUse();
+                shopuse.shopping_car = Session["shopping_car"] as List<int>;
+                if (Session["shopping_car"] != null)
+                {
+                    shopuse.products = shopuse.product_account(Session["shopping_car"] as List<int>);
+                }
+                return View(shopuse);
+            }
+            else
+            {
+                Session["shopping_car"] = null;
+                return RedirectToAction("Index", "Shop", null);
+            }
         }
-        [HttpPost]
-        public ActionResult Accounting(List<int> id)
+        public ActionResult Getsession(List<int> id)
         {
             Session["shopping_car"] = id;
             return new EmptyResult();
--- InterViewUse/InterViewUse/Models/ShopUse.cs
+++ InterViewUse/InterViewUse/Models/ShopUse.cs
@@ -10,5 +10,14 @@ namespace InterViewUse.Models
         public IEnumerable<Categories> categories { get; set; }
         public IEnumerable<Products> products { get; set; }
         public List<int> shopping_car { get; set; }
+        public List<Products> product_account(List<int> account)
+        { Repository<Products> RP = new Repository<Products>();
+            List<Products> LP = new List<Products>();
+            for(int i = 0;i<account.Count;i+=1)
+            {
+                LP.Add(RP.GetByID(account[i]));
+            }
+            return LP;
+        }
     }
 }
